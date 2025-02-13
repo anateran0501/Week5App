a6729d4b8f69876b17b2855364166d92cac1a79b
Ana T. <163786190+anateran0501@users.noreply.github.com>
2025-02-13 09:51:38 -0500

    Update streamlitapp_py.py
    Update on code

--- streamlitapp_py.py
+++ streamlitapp_py.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1q701-Tb8BlXijAkrJaQLLiGgliOivXyM
 """
 
-!pip install streamlit
+pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
