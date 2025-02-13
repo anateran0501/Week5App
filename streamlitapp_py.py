25ce532e4440bba98991224011d3ccdfd196ad11
Ana T. <163786190+anateran0501@users.noreply.github.com>
2025-02-13 09:47:43 -0500

    Update streamlitapp_py.py
    Added module installation

--- streamlitapp_py.py
+++ streamlitapp_py.py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1q701-Tb8BlXijAkrJaQLLiGgliOivXyM
 """
 
+!pip install streamlit
 import streamlit as st
 import pandas as pd
 import numpy as np
